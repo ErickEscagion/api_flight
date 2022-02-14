6d0595ddd3d23fac6aa0fa61a3afd6a8754cf471
Erick Escagion <erickeescagion@gmail.com>
2022-02-14 18:48:32 -0300

    project started

---
+++ src/main/java/folks/api_flight/ApiFlightApplication.java
@@ -0,0 +1,13 @@
+package folks.api_flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/folks/api_flight/ApiFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package folks.api_flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
