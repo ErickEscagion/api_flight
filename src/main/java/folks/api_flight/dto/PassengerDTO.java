7ed817277a17da594b1a3e81d250b3852fc1dee3
Erick Escagion <erickeescagion@gmail.com>
2022-02-14 20:11:45 -0300

    created Passenger entity/DTO

---
+++ src/main/java/folks/api_flight/dto/PassengerDTO.java
@@ -0,0 +1,42 @@
+package folks.api_flight.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import folks.api_flight.entities.BaseUser;
+import folks.api_flight.entities.Passenger;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class PassengerDTO extends BaseUser {
+
+    @NotBlank(message = "The emergency phone number is mandatory!")
+    private String emergencyPhoneNumber;
+
+    public PassengerDTO(
+            Long id,
+            @NotBlank(message = "The user name is mandatory!") String name,
+            @NotBlank(message = "Email is mandatory!") @Email String email,
+            @NotBlank(message = "The phone number is mandatory!") String phoneNumber,
+            @NotBlank(message = "The address is mandatory!") String address,
+            @NotBlank(message = "The emergency phone number is mandatory!") String emergencyPhoneNumber) {
+        super(id, name, email, phoneNumber, address);
+        this.emergencyPhoneNumber = emergencyPhoneNumber;
+    }
+
+    public PassengerDTO(Passenger passenger) {
+        this.setId(passenger.getId());
+        this.setName(passenger.getName());
+        this.setEmail(passenger.getEmail());
+        this.setPhoneNumber(passenger.getPhoneNumber());
+        this.setAddress(passenger.getAddress());
+        this.emergencyPhoneNumber = passenger.getEmergencyPhoneNumber();
+    }
+
+}
---
+++ src/main/java/folks/api_flight/entities/Passenger.java
@@ -0,0 +1,50 @@
+package folks.api_flight.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import folks.api_flight.dto.PassengerDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_PASSENGER")
+@PrimaryKeyJoinColumn(name = "PASSENGER_ID")
+public class Passenger extends BaseUser {
+
+    @NotBlank(message = "The emergency phone number is mandatory!")
+    private String emergencyPhoneNumber;
+
+    public Passenger(
+            Long id,
+            @NotBlank(message = "The user name is mandatory!") String name,
+            @NotBlank(message = "Email is mandatory!") @Email String email,
+            @NotBlank(message = "The phone number is mandatory!") String phoneNumber,
+            @NotBlank(message = "The address is mandatory!") String address,
+            @NotBlank(message = "The emergency phone number is mandatory!") String emergencyPhoneNumber) {
+        super(id, name, email, phoneNumber, address);
+        this.emergencyPhoneNumber = emergencyPhoneNumber;
+    }
+
+    public Passenger(Passenger passenger) {
+        this.emergencyPhoneNumber = passenger.getEmergencyPhoneNumber();
+    }
+
+    public Passenger(PassengerDTO dto) {
+        this.setId(dto.getId());
+        this.setName(dto.getName());
+        this.setEmail(dto.getEmail());
+        this.setPhoneNumber(dto.getPhoneNumber());
+        this.setAddress(dto.getAddress());
+        this.emergencyPhoneNumber = dto.getEmergencyPhoneNumber();
+    }
+}
