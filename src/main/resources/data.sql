f2fd0c1992b6fe5b64b2ca7e25358bbe09a44b50
Erick Escagion <erickeescagion@gmail.com>
2022-02-15 22:00:04 -0300

    inserted data crew

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,13 @@
 INSERT INTO TB_BASEUSER(name, email,phone_number,address) VALUES('Erick', 'erick@gmail.com', '(15)99818-1242', 'rua do erick, 123');
-INSERT INTO TB_PASSENGER(passenger_id, emergency_phone_number) VALUES (1, '(15)99716-3158');
+INSERT INTO TB_BASEUSER(name, email,phone_number,address) VALUES('Ana', 'ana@gmail.com', '(95)12475-1242', 'rua da ana, 22');
+INSERT INTO TB_BASEUSER(name, email,phone_number,address) VALUES('Laura', 'laura@outlook.com', '(11)99555-6652', 'rua da lau, 14523');
+INSERT INTO TB_BASEUSER(name, email,phone_number,address) VALUES('Pedro', 'pe@outlook.com', '(11)92626-6132', 'rua pedrao, 95425');
+INSERT INTO TB_BASEUSER(name, email,phone_number,address) VALUES('Paulo', 'pa@outlook.com', '(11)95659-1123', 'paulo ruA, 3');
+
+INSERT INTO TB_PASSENGER(passenger_id, emergency_phone_number) VALUES (4, '(15)99716-3158');
+INSERT INTO TB_PASSENGER(passenger_id, emergency_phone_number) VALUES (5, '(15)70700-7070');
+
+INSERT INTO TB_CREW(level,office,salary,crew_id) VALUES ('INTERN','PILOT', 3500.50, 1);
+INSERT INTO TB_CREW(level,office,salary,crew_id) VALUES ('PLENO','FLIGHTATTENDANT', 7000.99, 2);
+INSERT INTO TB_CREW(level,office,salary,crew_id) VALUES ('SENIOR','PILOT', 22590.90, 3);
+
